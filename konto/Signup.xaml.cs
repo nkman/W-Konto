723341cc3113d9720e5a5ca8b23e2198f905f3f3
Nairitya Khilari <nkmann2@gmail.com>
2015-01-05 13:08:29 +0530

    Add: http req sender.

--- konto/Signup.xaml.cs
+++ konto/Signup.xaml.cs
@@ -8,11 +8,15 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Newtonsoft.Json;
+using konto;
+using System.IO;
+using System.Text;
 
 namespace konto
 {
     public partial class Signup : PhoneApplicationPage
     {
+        public static string json;
         public Signup()
         {
             InitializeComponent();
@@ -40,6 +44,7 @@ namespace konto
             string _username = username.Text;
             string _password = password.Password;
 
+            var config = new urlConfig();
             var dataToSend = new DataObject();
             dataToSend.data = new List<Item>{
                 new Item {
@@ -50,11 +55,57 @@ namespace konto
                 }
             };
 
-            string json = JsonConvert.SerializeObject(dataToSend.data);
+            json = JsonConvert.SerializeObject(dataToSend.data);
             
             //HttpWebRequest 
-            
-            System.Diagnostics.Debug.WriteLine(json);
+            string a = config.signupUrl();
+            System.Diagnostics.Debug.WriteLine(a);
+            PostJsonRequest();
+        }
+
+
+        private void PostJsonRequest()
+        {
+            var config = new urlConfig();
+            string AuthServiceUri = config.signupUrl(); 
+            HttpWebRequest spAuthReq = HttpWebRequest.Create(AuthServiceUri) as HttpWebRequest;
+            spAuthReq.ContentType = "application/json";
+            spAuthReq.Method = "POST";
+            spAuthReq.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), spAuthReq);
+        }
+
+        void GetRequestStreamCallback(IAsyncResult callbackResult)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)callbackResult.AsyncState;
+            Stream postStream = myRequest.EndGetRequestStream(callbackResult);
+            string postData = json;
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            postStream.Write(byteArray, 0, byteArray.Length);
+            postStream.Close();
+            myRequest.BeginGetResponse(new AsyncCallback(GetResponsetStreamCallback), myRequest);
+        }
+
+        void GetResponsetStreamCallback(IAsyncResult callbackResult)
+        {
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)callbackResult.AsyncState;
+                HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(callbackResult);
+                string responseString = "";
+                Stream streamResponse = response.GetResponseStream();
+                StreamReader reader = new StreamReader(streamResponse);
+                responseString = reader.ReadToEnd();
+                streamResponse.Close();
+                reader.Close();
+                response.Close();
+                string result = responseString;
+                System.Diagnostics.Debug.WriteLine("Okay "+result);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("NOOOO "+ e.ToString());
+            }
         }
     }
 
