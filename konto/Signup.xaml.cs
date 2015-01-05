7e4be17add3a02dbd33e98ebbafa209b7bdd3935
Nairitya Khilari <nkmann2@gmail.com>
2015-01-05 10:16:02 +0530

    Add: Json serialization from list.
    Added NewtonSoft Json Library

--- konto/Signup.xaml.cs
+++ konto/Signup.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Newtonsoft.Json;
 
 namespace konto
 {
@@ -15,6 +16,45 @@ namespace konto
         public Signup()
         {
             InitializeComponent();
+        }
+
+        public class Item
+        {
+
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+        public class DataObject
+        {
+            public List<Item> data { get; set; }
+        }
+
+        public void SignupUser(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Sign up");
+            string _firstname = firstname.Text;
+            string _lastname = lastname.Text;
+            string _username = username.Text;
+            string _password = password.Password;
+
+            var dataToSend = new DataObject();
+            dataToSend.data = new List<Item>{
+                new Item {
+                     username = _username,
+                     firstname = _firstname,
+                     lastname = _lastname,
+                     password = _password
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(dataToSend.data);
+            
+            //HttpWebRequest 
+            
+            System.Diagnostics.Debug.WriteLine(json);
         }
     }
 }
