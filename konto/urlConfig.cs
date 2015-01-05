dfa6bb9e25190311d47a02a752d753a99130af2f
Nairitya Khilari <nkmann2@gmail.com>
2015-01-05 12:23:04 +0530

    Add: url configuration

---
+++ konto/urlConfig.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konto
+{
+    class urlConfig
+    {
+        public string homeUrl()
+        {
+            string homeurl = "http://shyamu.herokuapp.com";
+            return homeurl;
+        }
+
+        public string signupUrl()
+        {
+            string signupurl = "/mobile/signup";
+            return homeUrl() + signupurl;
+        }
+
+        public string loginUrl()
+        {
+            string loginurl = "/mobile/login";
+            return homeUrl() + loginurl;
+        }
+
+        public string notificationUrl()
+        {
+            string notificationurl = "/mobile/notification";
+            return homeUrl() + notificationurl;
+        }
+
+        public string logOutUrl()
+        {
+            string logouturl = "/";
+            return homeUrl() + logouturl;
+        }
+
+        public string addUrl()
+        {
+            string addurl = "/mobile/add";
+            return homeUrl() + addurl;
+        }
+
+        public string notificationReadUrl()
+        {
+            string notificationreadurl = "/mobile/notification/read";
+            return homeUrl() + notificationreadurl;
+        }
+
+        public string notificationDeleteUrl()
+        {
+            string notificationdeleteurl = "/mobile/notification/delete";
+            return homeUrl() + notificationdeleteurl;
+        }
+
+        public string notificationDeclineUrl()
+        {
+            string notificationdeclineurl = "/mobile/notification/decline";
+            return homeUrl() + notificationdeclineurl;
+        }
+
+        public string notificationAcceptUrl()
+        {
+            string notificationaccepturl = "/mobile/notification/accept";
+            return homeUrl() + notificationaccepturl;
+        }
+
+    }
+}
