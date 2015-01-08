3fd528c3ebe6acaf74d4a7cef967a8b85cff0c5d
Nairitya Khilari <nkmann2@gmail.com>
2015-01-08 19:36:23 +0530

    Add: notification table schema.

--- konto/dbCollection.cs
+++ konto/dbCollection.cs
@@ -107,6 +107,186 @@ namespace konto
     }
 
     [Table]
+    public class Notification : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+
+        /*
+         * NoticeId - int,
+         * Name - string(can be NUll),
+         * Notice_id - string,
+         * IsPositive - bool (can be Null),
+         * IsNegetive - bool (can be Null),
+         * IsTracking - bool (can be Null),
+         * Amount - int (can be Null)
+         * */
+        private int _noticeId;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int NoticeId
+        {
+            get
+            {
+                return _noticeId;
+            }
+            set
+            {
+                if (_noticeId != value)
+                {
+                    NotifyPropertyChanging("NoticeId");
+                    _noticeId = value;
+                    NotifyPropertyChanged("NoticeId");
+                }
+            }
+        }
+
+        private string _name;
+        [Column(CanBeNull = true)]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    NotifyPropertyChanging("Name");
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string _notice_id;
+        [Column]
+        public string Notice_id
+        {
+            get
+            {
+                return _notice_id;
+            }
+            set
+            {
+                if (_notice_id != value)
+                {
+                    NotifyPropertyChanging("Name");
+                    _notice_id = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private bool _isPositive;
+        [Column(CanBeNull = true)]
+        public bool IsPositive
+        {
+            get
+            {
+                return _isPositive;
+            }
+            set
+            {
+                if (_isPositive != value)
+                {
+                    NotifyPropertyChanging("IsPositive");
+                    _isPositive = value;
+                    NotifyPropertyChanged("IsPositive");
+                }
+            }
+        }
+
+        private bool _isNegetive;
+        [Column(CanBeNull = true)]
+        public bool IsNegetive
+        {
+            get
+            {
+                return _isNegetive;
+            }
+            set
+            {
+                if (_isNegetive != value)
+                {
+                    NotifyPropertyChanging("IsNegetive");
+                    _isNegetive = value;
+                    NotifyPropertyChanged("IsNegetive");
+                }
+            }
+        }
+
+        private bool _isTracking;
+        [Column(CanBeNull = true)]
+        public bool IsTracking
+        {
+            get
+            {
+                return _isTracking;
+            }
+            set
+            {
+                if (_isTracking != value)
+                {
+                    NotifyPropertyChanging("IsTracking");
+                    _isTracking = value;
+                    NotifyPropertyChanged("IsTracking");
+                }
+            }
+        }
+
+        private int _amount;
+        [Column(CanBeNull = true)]
+        public int Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                if (_amount != value)
+                {
+                    NotifyPropertyChanging("Amount");
+                    _amount = value;
+                    NotifyPropertyChanged("Amount");
+                }
+            }
+        }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the page that a data context property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify the data context that a data context property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+
+
+
+
+    [Table]
     public class User : INotifyPropertyChanged, INotifyPropertyChanging
     {
         // Define ID: private field, public property and database column.
